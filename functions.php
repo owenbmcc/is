12ec77f037ac12b27ea38dd06d6b269d3d40b8a9
owenbmcc <oroberts@bmcc.cuny.edu>
2018-11-21 17:14:44 -0500

    some bug fixes

--- functions.php
+++ functions.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * Require the wp_bootstrap_navwalker file that will allow for Bootstrap nav menus to be used with the built-in WP menu system
- */
-require get_template_directory() . '/vendor/wp_bootstrap_navwalker.php';
-
-
-// Register Custom Navigation Walker
-//require_once('bs4navwalker.php');
-require get_template_directory() . '/vendor/bs4navwalker.php';
-
 
 // Add in support for featured images, custom image sizes, set default nav menu, 
 function bare_bones_setup() {
@@ -63,37 +53,5 @@ $args = array(
 	'uploads'       => true,
 );
 add_theme_support( 'custom-header', $args );
-
-
-/*
- * ADD IN EXTERNAL CSS AND JS (BOOTSTRAP and others)
- */
-
-function bare_bones_bootstrap_enqueue_scripts() {
-	$template_url = get_template_directory_uri();
-	// jQuery.
-	wp_enqueue_script( 'jquery' );
-    
-	// Bootstrap
-//	wp_register_script( 'bootstrap-script', $template_url . '/js/bootstrap.min.js', array( 'jquery' ), '1.0.0', true );
-    //wp_register_script( 'stellar-script', $template_url. '/js/jquery.stellar.min.js', array( 'jquery' ), '1.0.0',true );
-    wp_register_script('unveilEffects', $template_url .'/js/jquery.unveilEffects.js',array('jquery'), '1.0.0',true);
-    wp_register_script('theme', $template_url .'/js/theme.js', array('jquery'),'1.0.0',true);
-    //wp_register_script( 'retina', $template_url . '/js/retina-1.1.0.js', array( 'jquery' ), '1.0.0', true );
-    
-	wp_enqueue_style( 'bootstrap-style', $template_url . '/css/bootstrap.min.css' );
-	//Main Style
-    
-	wp_enqueue_style( 'main-style', get_stylesheet_uri() );
-	// Load Thread comments WordPress script.
-	if ( is_singular() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'bare_bones_bootstrap_enqueue_scripts', 1 );
-
-
-
-
 
 ?>
--- header.php
+++ header.php
@@ -1,58 +1,24 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="img/favicon.ico">
-    <title><?php wp_title(); ?></title>
-
-<!--     Custom styles for this template -->
-    <link href="<?php echo get_template_directory_uri(); ?>/css/ionicons.min.css" rel="stylesheet">
-
-  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/cubeportfolio.css" />
-   
-    <link href="<?php echo get_template_directory_uri(); ?>/css/custom-animations.css" rel="stylesheet">
-
-  <script src="<?php echo get_template_directory_uri(); ?>/js/modernizr.min.js"></script>
-
-    
-    <?php wp_head(); ?>
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.cubeportfolio.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.unveilEffects.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/theme.js"></script>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="img/favicon.ico">
+	<title><?php wp_title(); ?></title>
+	 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-    
-    <!--    Add in WordPress Navigation -->
-
-   <nav class="navbar navbar-light navbar-toggleable-md fixed-top" role="navigation">
-     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-    <a class="navbar-brand" href="http://bmcc.is"><img src="<?php echo get_template_directory_uri(); ?>/img/mea-Logo2.PNG" width="40"></a>
-<!--   New walker attempt-->
-   <?php
-   wp_nav_menu([
-     'menu'            => 'main',
-     'theme_location'  => 'main',
-     'container'       => 'div',
-     'container_id'    => 'bs4navbar',
-     'container_class' => 'collapse navbar-collapse',
-     'menu_id'         => false,
-     'menu_class'      => 'navbar-nav mr-auto',
-     'depth'           => 2,
-     'fallback_cb'     => 'bs4navwalker::fallback',
-     'walker'          => new bs4navwalker()
-   ]);
-   ?>
-   
-    </nav>
+	<!--    Add in WordPress Navigation -->
+	<nav class="navbar navbar-light navbar-toggleable-md fixed-top" role="navigation">
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<a class="navbar-brand" href="http://bmcc.is"><img src="<?php echo get_template_directory_uri(); ?>/img/mea-Logo2.PNG" width="40"></a>
+	</nav>
     
